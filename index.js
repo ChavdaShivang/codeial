3969b417ebff923ffc2220f7d2af433018c327ab
Chavda Shivang <chavdashivang3203@gmail.com>
2024-03-14 15:22:38 +0530

    add layouts and partial views to our project

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const port = 8000;
 const app = express();
+const expressLayouts = require('express-ejs-layouts');
 
+
+app.use(express.static('./assets'));
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+
+app.use(expressLayouts);
 app.use("/", require('./routes/index'));
 app.set("view engine", "ejs");
 app.set("views", "./views");
